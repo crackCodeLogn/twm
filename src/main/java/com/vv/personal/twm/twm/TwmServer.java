de6eb2d45c343630f1d0190a1236faed9f283389
Vivek <vivektheintel@gmail.com>
2020-12-06 21:25:38 +0530

    Upgrading to swagger 3.0.0

--- src/main/java/com/vv/personal/twm/twm/TwmServer.java
+++ src/main/java/com/vv/personal/twm/twm/TwmServer.java
@@ -6,13 +6,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Vivek
  * @since 16/11/20
  */
-@EnableSwagger2
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
