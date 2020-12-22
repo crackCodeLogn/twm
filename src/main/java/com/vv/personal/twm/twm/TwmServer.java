3f6439a2ba9a10762c6371e9846369283770737a
Vivek <vivektheintel@gmail.com>
2020-12-22 22:12:56 +0530

    Removing noise in swagger

--- src/main/java/com/vv/personal/twm/twm/TwmServer.java
+++ src/main/java/com/vv/personal/twm/twm/TwmServer.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * @author Vivek
@@ -22,5 +25,13 @@ public class TwmServer {
     @Bean
     ProtobufHttpMessageConverter protobufHttpMessageConverter() {
         return new ProtobufHttpMessageConverter();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vv.personal.twm.twm"))
+                .build();
     }
 }
