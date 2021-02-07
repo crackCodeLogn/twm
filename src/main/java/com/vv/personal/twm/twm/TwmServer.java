152295574ee399c88077575e4d360c05115361a7
Vivek <vivektheintel@gmail.com>
2021-02-07 13:21:44 +0530

    Adding in component scan

--- src/main/java/com/vv/personal/twm/twm/TwmServer.java
+++ src/main/java/com/vv/personal/twm/twm/TwmServer.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
@@ -27,6 +28,7 @@ import static com.vv.personal.twm.twm.constants.Constants.*;
  */
 @EnableFeignClients
 @EnableEurekaClient
+@ComponentScan({"com.vv.personal.twm.twm", "com.vv.personal.twm.ping"})
 @SpringBootApplication
 public class TwmServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(TwmServer.class);
