98e994ec28763c13a7c9e67a3e215aecdfbe2f18
Vivek <vivektheintel@gmail.com>
2024-01-24 12:27:56 -0500

    Updating jdk path

--- bin/twm.sh
+++ bin/twm.sh
@@ -4,7 +4,7 @@ JAVA_PARAM="-Xmx2g"
 
 BIN_PATH=$TWM_HOME_PARENT/TWM/$APP_NAME/bin     #TWM-HOME-PARENT :: exported in .bashrc
 JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
-JAVA_PATH=$HOME/.jdks/corretto-17.0.9/bin/java
+JAVA_PATH=$HOME/.jdks/jdk17/bin/java
 
 echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', at '$JAR_PATH'"
 $JAVA_PATH $JAVA_PARAM -jar $JAR_PATH
